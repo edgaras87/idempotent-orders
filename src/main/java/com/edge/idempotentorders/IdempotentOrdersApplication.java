f782495ba7022d15490a1d9825daa12542b0bfad
edgaras87 <stasiukaitisedgaras87@gmail.com>
2026-07-24 20:32:06 +0300

    define(src): bootstrap Spring Boot application skeleton

---
+++ src/main/java/com/edge/idempotentorders/IdempotentOrdersApplication.java
@@ -0,0 +1,13 @@
+package com.edge.idempotentorders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdempotentOrdersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdempotentOrdersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edge/idempotentorders/IdempotentOrdersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edge.idempotentorders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdempotentOrdersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
